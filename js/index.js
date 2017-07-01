4fe86714062ea3f441ffb88430f2a28136cc6e3d
Traci Ruffner <traci.ruffner@FASTLANEUS.COM>
2017-07-01 01:04:41 -0400

    added carousels

--- js/index.js
+++
@@ -1,4 +0,0 @@
-var pictures = ["A", "B", "C", "D"];
-var words = ["apple", "ball", "car", "dog"];
-
-<button type="button" class="btn btn-warning" onclick='document.getElementById("pictures").innerHTML = "#">Click Me!</button>
