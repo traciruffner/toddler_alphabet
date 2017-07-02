07650499cf478b4891c8049163fb9ebd52d6530a
Traci Ruffner <traci.ruffner@FASTLANEUS.COM>
2017-07-01 21:50:59 -0400

    completed project

--- js/index.js
+++ js/index.js
@@ -24,6 +24,345 @@ $(document).ready(function(){
      $(this).hide(function(){
       $('#B').show();
    });
-});
+ });
+    
+    $('#altC').hide();
+    
+   $('#C').click(function(){
+      $(this).hide(function(){
+        $('#altC').show();
+      }); 
+   }); 
+    
+   $('#altC').click(function(){
+     $(this).hide(function(){
+      $('#C').show();
+   });
+ });
+    
+    $('#altD').hide();
+    
+   $('#D').click(function(){
+      $(this).hide(function(){
+        $('#altD').show();
+      }); 
+   }); 
+    
+   $('#altD').click(function(){
+     $(this).hide(function(){
+      $('#D').show();
+   });
+ });
+    
+    $('#altE').hide();
+    
+   $('#E').click(function(){
+      $(this).hide(function(){
+        $('#altE').show();
+      }); 
+   }); 
+    
+   $('#altE').click(function(){
+     $(this).hide(function(){
+      $('#E').show();
+   });
+ });
+    
+    $('#altF').hide();
+    
+   $('#F').click(function(){
+      $(this).hide(function(){
+        $('#altF').show();
+      }); 
+   }); 
+    
+   $('#altF').click(function(){
+     $(this).hide(function(){
+      $('#F').show();
+   });
+ });
+    
+    $('#altG').hide();
+    
+   $('#G').click(function(){
+      $(this).hide(function(){
+        $('#altG').show();
+      }); 
+   }); 
+    
+   $('#altG').click(function(){
+     $(this).hide(function(){
+      $('#G').show();
+   });
+ });
+    
+    $('#altH').hide();
+    
+   $('#H').click(function(){
+      $(this).hide(function(){
+        $('#altH').show();
+      }); 
+   }); 
+    
+   $('#altH').click(function(){
+     $(this).hide(function(){
+      $('#H').show();
+   });
+ });
+    
+    $('#altI').hide();
+    
+   $('#I').click(function(){
+      $(this).hide(function(){
+        $('#altI').show();
+      }); 
+   }); 
+    
+   $('#altI').click(function(){
+     $(this).hide(function(){
+      $('#I').show();
+   });
+ });
+    
+    $('#altJ').hide();
+    
+   $('#J').click(function(){
+      $(this).hide(function(){
+        $('#altJ').show();
+      }); 
+   }); 
+    
+   $('#altJ').click(function(){
+     $(this).hide(function(){
+      $('#J').show();
+   });
+ });
+    
+    $('#altK').hide();
+    
+   $('#K').click(function(){
+      $(this).hide(function(){
+        $('#altK').show();
+      }); 
+   }); 
+    
+   $('#altK').click(function(){
+     $(this).hide(function(){
+      $('#K').show();
+   });
+ });
+    
+     $('#altL').hide();
+    
+   $('#L').click(function(){
+      $(this).hide(function(){
+        $('#altL').show();
+      }); 
+   }); 
+    
+   $('#altL').click(function(){
+     $(this).hide(function(){
+      $('#L').show();
+   });
+ });
+    
+    $('#altM').hide();
+    
+   $('#M').click(function(){
+      $(this).hide(function(){
+        $('#altM').show();
+      }); 
+   }); 
+    
+   $('#altM').click(function(){
+     $(this).hide(function(){
+      $('#M').show();
+   });
+ });
+    
+    $('#altN').hide();
+    
+   $('#N').click(function(){
+      $(this).hide(function(){
+        $('#altN').show();
+      }); 
+   }); 
+    
+   $('#altN').click(function(){
+     $(this).hide(function(){
+      $('#N').show();
+   });
+ });
+    
+    $('#altO').hide();
+    
+   $('#O').click(function(){
+      $(this).hide(function(){
+        $('#altO').show();
+      }); 
+   }); 
+    
+   $('#altO').click(function(){
+     $(this).hide(function(){
+      $('#O').show();
+   });
+ });
+     
+    $('#altP').hide();
+    
+   $('#P').click(function(){
+      $(this).hide(function(){
+        $('#altP').show();
+      }); 
+   }); 
+    
+   $('#altP').click(function(){
+     $(this).hide(function(){
+      $('#P').show();
+   });
+ });
+    
+    $('#altQ').hide();
+    
+   $('#Q').click(function(){
+      $(this).hide(function(){
+        $('#altQ').show();
+      }); 
+   }); 
+    
+   $('#altQ').click(function(){
+     $(this).hide(function(){
+      $('#Q').show();
+   });
+ });
+    
+    $('#altR').hide();
+    
+   $('#R').click(function(){
+      $(this).hide(function(){
+        $('#altR').show();
+      }); 
+   }); 
+    
+   $('#altR').click(function(){
+     $(this).hide(function(){
+      $('#R').show();
+   });
+ });
+    
+    $('#altS').hide();
+    
+   $('#S').click(function(){
+      $(this).hide(function(){
+        $('#altS').show();
+      }); 
+   }); 
+    
+   $('#altS').click(function(){
+     $(this).hide(function(){
+      $('#S').show();
+   });
+ });
+    
+    $('#altT').hide();
+    
+   $('#T').click(function(){
+      $(this).hide(function(){
+        $('#altT').show();
+      }); 
+   }); 
+    
+   $('#altT').click(function(){
+     $(this).hide(function(){
+      $('#T').show();
+   });
+ });
+    
+    $('#altU').hide();
+    
+   $('#U').click(function(){
+      $(this).hide(function(){
+        $('#altU').show();
+      }); 
+   }); 
+    
+   $('#altU').click(function(){
+     $(this).hide(function(){
+      $('#U').show();
+   });
+ });
+    
+    $('#altV').hide();
+    
+   $('#V').click(function(){
+      $(this).hide(function(){
+        $('#altV').show();
+      }); 
+   }); 
+    
+   $('#altV').click(function(){
+     $(this).hide(function(){
+      $('#V').show();
+   });
+ });
+    
+    $('#altW').hide();
+    
+   $('#W').click(function(){
+      $(this).hide(function(){
+        $('#altW').show();
+      }); 
+   }); 
+    
+   $('#altW').click(function(){
+     $(this).hide(function(){
+      $('#W').show();
+   });
+ });
+    
+    $('#altX').hide();
+    
+   $('#X').click(function(){
+      $(this).hide(function(){
+        $('#altX').show();
+      }); 
+   }); 
+    
+   $('#altX').click(function(){
+     $(this).hide(function(){
+      $('#X').show();
+   });
+ });
+    
+    $('#altY').hide();
+    
+   $('#Y').click(function(){
+      $(this).hide(function(){
+        $('#altY').show();
+      }); 
+   }); 
+    
+   $('#altY').click(function(){
+     $(this).hide(function(){
+      $('#Y').show();
+   });
+ });
+    
+   $('#altZ').hide();
+    
+   $('#Z').click(function(){
+      $(this).hide(function(){
+        $('#altZ').show();
+      }); 
+   }); 
+    
+   $('#altZ').click(function(){
+     $(this).hide(function(){
+      $('#Z').show();
+   });
+ });
+    
+     
+
     
 });
